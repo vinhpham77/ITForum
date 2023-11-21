d26db4a988eacfbe2575dacc71b91c99eea19765
Vin <phamvanvinh8a2@gmail.com>
2023-11-21 13:48:34 +0700

    convert DioException

--- lib/ui/common/utils.dart
+++ lib/ui/common/utils.dart
@@ -1,15 +1,16 @@
 import 'package:dio/dio.dart';
 
-String getMessageFromException(DioException err) {
+String getMessageFromException(Exception err) {
+  DioException error = err as DioException;
   String message = '';
 
-  if (err.response?.data is Map<String, dynamic>) {
-    Map<String, dynamic> data = err.response?.data;
+  if (error.response?.data is Map<String, dynamic>) {
+    Map<String, dynamic> data = error.response?.data;
     message = data.entries
         .map((entry) => "${entry.key}: ${entry.value}")
         .join("\n");
-  } else if (err.response?.data is String) {
-    message = err.response?.data;
+  } else if (error.response?.data is String) {
+    message = error.response?.data;
   } else {
     message = "Có lỗi xảy ra. Vui lòng thử lại sau!";
   }
