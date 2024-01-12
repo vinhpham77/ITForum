9a988db76963f9ef5a60bdf6e2715279c605faf4
Luong <luongnguyen.270902@gmail.com>
2024-01-12 11:04:18 +0700

    update

--- lib/ui/views/posts/widgets/right_page/right_item.dart
+++ lib/ui/views/posts/widgets/right_page/right_item.dart
@@ -19,9 +19,6 @@ class RightItem extends StatelessWidget {
         children: [
           InkWell(
             onTap: () => {appRouter.go('/posts/${postAggregation.id}', extra: {})},
-            child: Text(postAggregation.title,
-              style: TextStyle(fontSize: 16),
-            onTap: () => appRouter.push('/posts/${postAggregation.user?.id}', extra: {}),
             child: Text(postAggregation.title!,
               style: const TextStyle(fontSize: 16),
               softWrap: true,
@@ -39,8 +36,9 @@ class RightItem extends StatelessWidget {
             ],
           ),
           InkWell(
-            onTap: () => appRouter.go('/profile/${postAggregation.user.username}', extra: {}),
-            child: Text(postAggregation.user.displayName,
+            onTap: () => appRouter
+                .go('/profile/${postAggregation.user?.username}', extra: {}),
+            child: Text(postAggregation.user!.displayName,
               style: TextStyle(color: Colors.black38),
             ),
           ),
