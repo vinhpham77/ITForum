abfefbb7bbc9875df8c309a678e3ed166dad6404
Vin <phamvanvinh8a2@gmail.com>
2024-01-14 04:52:06 +0700

    profile personal

--- lib/ui/views/profile/blocs/personal_tab/personal_tab_bloc.dart
+++ lib/ui/views/profile/blocs/personal_tab/personal_tab_bloc.dart
@@ -1,17 +1,12 @@
-import 'package:cay_khe/dtos/bookmark_item.dart';
 import 'package:cay_khe/repositories/user_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../../dtos/result_count.dart';
 import '../../../../../models/user.dart';
-import '../../../../../repositories/bookmark_repository.dart';
-import '../../../../common/utils/message_from_exception.dart';
 
 part 'personal_tab_event.dart';
-
 part 'personal_tab_state.dart';
 
 class PersonalTabBloc extends Bloc<PersonalTabEvent, PersonalTabState> {
